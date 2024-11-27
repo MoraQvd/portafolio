e215cfce33ee45c560cdf2133866ff4bae6fe224
MoraQvd <mora.quevedo.7e8@itb.cat>
2024-11-27 10:33:35 +0100

    Create repositorio.js

---
+++ repositorio.js
@@ -0,0 +1,8 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach((section) => {
+        section.classList.remove('active');
+    });
+    const activeSection = document.getElementById(sectionId);
+    activeSection.classList.add('active');
+}
