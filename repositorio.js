a879e3393cc3ed13babea5fa4842fe1c72377513
mora.quevedo.7e8 <mora.quevedo.7e8@itb.cat>
2024-11-30 13:30:43 +0100

    mora

--- repositorio.js
+++ repositorio.js
@@ -6,3 +6,36 @@ function showSection(sectionId) {
     const activeSection = document.getElementById(sectionId);
     activeSection.classList.add('active');
 }
+
+function copyText() {
+    // Obtén el contenido del div con id "correo_autor"
+    const textElement = document.getElementById("correo_autor");
+
+    // Crea un rango y selecciona el texto
+    const range = document.createRange();
+    range.selectNodeContents(textElement);
+
+    // Selecciona el texto
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+
+    // Intenta copiar el texto al portapapeles
+    try {
+        document.execCommand('copy');
+
+        // Muestra el mensaje de confirmación
+        const confirmacion_mensaje = document.getElementById("confirmacion_mensaje");
+        confirmacion_mensaje.style.display = "block";
+
+        // Ocultar el mensaje después de 2 segundos
+        setTimeout(() => {
+            confirmacion_mensaje.style.display = "none";
+        }, 2000);
+    } catch (err) {
+        console.error("No se pudo copiar el texto", err);
+    }
+
+    // Limpia la selección
+    window.getSelection().removeAllRanges();
+}
+
